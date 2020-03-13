12f49f364daa8e3c9f01b143fd3c8ca2398a3e0a
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-03-13 15:27:25 +0300

    rename services

--- lab1/app/app.js
+++ lab1/app/app.js
@@ -4,8 +4,8 @@ const services = require('./servicelayer')('./services');
 const router = require('./router.js')('./routes/', services);
 const app = express();
 app.use(router);
-const server = app.listen(services.config.port, () => {
-    services.log.info('*********************************');
-    services.log.info(`* App listening on port ${server.address().port}`);
-    services.log.info('*********************************');
+const server = app.listen(services.configService.port, () => {
+    services.logService.info('*********************************');
+    services.logService.info(`* App listening on port ${server.address().port}`);
+    services.logService.info('*********************************');
 });
