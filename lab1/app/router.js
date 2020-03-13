dc440d4a8827c71fef6354c644acbde266680f46
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-03-13 15:28:05 +0300

    add equation service api

--- lab1/app/router.js
+++ lab1/app/router.js
@@ -1,6 +1,9 @@
 'use strict';
 module.exports = (dir, services) => {
     const router = require('express').Router();
-    router.get('/', require(`${dir}/defaultRoute.js`)());
+    const defaultRoute = require(`${dir}/defaultRoute.js`)();
+    const solveEquationRoute = require(`${dir}/solveEquationRoute.js`)(services.equationService);
+    router.get('/', defaultRoute);
+    router.get('/equation', solveEquationRoute);
     return router;
 };
