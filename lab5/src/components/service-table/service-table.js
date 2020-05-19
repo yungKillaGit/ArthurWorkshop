7732f84403c223d5dfc0f306f899fbcaee1de21f
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-05-19 16:10:43 +0300

    add functionality to service table

--- lab5/src/components/service-table/service-table.js
+++ lab5/src/components/service-table/service-table.js
@@ -1,57 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import MaterialTable from 'material-table';
+import {Alert} from "@material-ui/lab";
 
 const ServiceTable = ({ services }) => {
   const [state, setState] = useState({
     columns: [
+      { title: 'ID услуги', field: 'id', editable: 'never' },
       { title: 'Название', field: 'name' },
       { title: 'Цена', field: 'price' },
       { title: 'Описание', field: 'description' },
     ],
     data: services,
   });
+  const [alertMessage, setAlertMessage] = useState();
+
+  const addService = (data) => axios.post('http://localhost:4000/api/services', data)
+    .then((response) => response.data)
+    .catch((error) => {
+      setAlertMessage(error.response.data.error);
+      return false;
+    });
+
+  const updateService = (data) => axios.put(`http://localhost:4000/api/services/${data.id}`, data)
+    .then((response) => response.data)
+    .catch((error) => {
+      setAlertMessage(error.response.data.error);
+      return false;
+    });
+
+  const deleteService = (data) => axios.delete(`http://localhost:4000/api/services/${data.id}`, data)
+    .then(() => true)
+    .catch((error) => {
+      setAlertMessage(error.response.data.error);
+      return false;
+    });
 
   return (
-    <MaterialTable
-      title="Services"
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: (newData) => new Promise((resolve) => {
-          setTimeout(() => {
+    <>
+      <MaterialTable
+        title="Services"
+        columns={state.columns}
+        data={state.data}
+        editable={{
+          onRowAdd: (newData) => new Promise((resolve) => {
             resolve();
-            setState((prevState) => {
-              const data = [...prevState.data];
-              data.push(newData);
-              return { ...prevState, data };
-            });
-          }, 600);
-        }),
-        onRowUpdate: (newData, oldData) => new Promise((resolve) => {
-          setTimeout(() => {
+            const data = [...state.data];
+            addService(newData)
+              .then((response) => {
+                if (response) {
+                  data.push(response);
+                }
+              })
+              .then(() => setState({ ...state, data }));
+          }),
+          onRowUpdate: (newData, oldData) => new Promise((resolve) => {
             resolve();
             if (oldData) {
-              setState((prevState) => {
-                const data = [...prevState.data];
-                data[data.indexOf(oldData)] = newData;
-                return { ...prevState, data };
-              });
+              const data = [...state.data];
+              updateService(newData)
+                .then((response) => {
+                  if (response) {
+                    data[data.indexOf(oldData)] = response;
+                  }
+                })
+                .then(() => setState({ ...state, data }));
             }
-          }, 600);
-        }),
-        onRowDelete: (oldData) => new Promise((resolve) => {
-          setTimeout(() => {
+          }),
+          onRowDelete: (oldData) => new Promise((resolve) => {
             resolve();
-            setState((prevState) => {
-              const data = [...prevState.data];
-              data.splice(data.indexOf(oldData), 1);
-              return { ...prevState, data };
-            });
-          }, 600);
-        }),
-      }}
-    />
+            const data = [...state.data];
+            deleteService(oldData)
+              .then((response) => {
+                if (response) {
+                  data.splice(data.indexOf(oldData), 1);
+                }
+              })
+              .then(() => setState({ ...state, data }));
+          }),
+        }}
+      />
+      {
+        alertMessage ? (
+          <Alert className="mt-2" severity="error" onClose={() => setAlertMessage(null)}>{alertMessage}</Alert>
+        ) : null
+      }
+    </>
   );
 };
 
